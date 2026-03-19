cccc7f12ef9ce785fec5e4b1cb102c43d977f6f5
James Kelley <jamesk3263827@gmail.com>
2026-03-19 10:53:44 -0400

    Set up Java Spring Boot waitlist service

---
+++ waitlist-service/src/main/java/waitlist_service/WaitlistServiceApplication.java
@@ -0,0 +1,13 @@
+package waitlist_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitlistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitlistServiceApplication.class, args);
+	}
+
+}
---
+++ waitlist-service/src/test/java/waitlist_service/WaitlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package waitlist_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
